b0659d1a41c12eb4a63b521c410df79984f83ada
Pez Cuckow <email@pezcuckow.com>
2017-05-25 16:23:58 +0100

    Move logging to a separate class

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-const colors = require('colors');
 const meetup = require('./lib/meetup');
 const twitter = require('./lib/twitter');
 const Cache = require('./lib/cache');
+const Log = require('./lib/log');
 
-const argv = require('minimist')(process.argv);
-const pollMinutes = (argv.p) ? argv.p : 60;
+const argv = require('minimist')(process.argv.slice(2));
 
 const twit = new twitter.createConnection({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -17,46 +16,51 @@ const meet = meetup.createConnection(process.env.MEETUP_API_KEY);
 
 function run() {
   const cache = new Cache();
-  console.log('info'.yellow + ' Contacting meetup.com');
-  console.log('info'.yellow + ' Requesting twitterIds for group: ' + process.env.MEETUP_GROUP_NAME);
+  Log.info('Contacting meetup.com');
+  Log.info('Requesting twitterIds for group: ' + process.env.MEETUP_GROUP_NAME);
   meet.getMemberTwitterIds(process.env.MEETUP_GROUP_NAME, function(err, resp) {
-    if(!err) {
-      console.log('success'.green + ' Retreived ' + resp.length + ' twitter names from meetup');
-      for(var i=0; i<resp.length; i++) {
-        // need to clean the names, some people write twitter.com/pezmc
-        let raw = resp[i];
-        if(raw.indexOf('\.com\/') !== -1) {
-          raw = raw.split('\.com\/')[1];
-        }
-        if(raw) cache.add('meetup', raw);
-      }
-      console.log('info'.yellow + ' Contacting twitter.com');
-      twit.getFollowedNames(function(err, resp) {
-        if(err) {
-          console.log('error'.red + ' ' + err.message); 
-        }
-          
-        console.log('success'.green + ' Retreived ' + resp.length + ' twitter names from twitter');
-        for(let i=0; i<resp.length; i++) {
-          cache.add('twitter', resp[i]);
-        }
-        let diff = cache.diff();
-        if(diff.length > 0) {
-          console.log('info'.yellow + 'Attempting to follow ' + diff.length + ' twitter names (' + diff.join() + ')');
-          twit.followNames(diff, function(err, resp) {
-            if(err) {
-              return console.log('error'.red + ' ' + err.message);
-            }
-
-            console.log('success'.green + ` All done! Waiting ${pollMinutes} minutes...`);
-          });
-        } else {
-          console.log('success'.green + ' Nobody to follow');
-        }
-      });
-    } else { 
-      console.log('error'.red + ' ' + err.message); 
+    if (err) {
+      Log.error(err.message);
+      return scheduleFollow(pollMinutes);
     }
+
+    Log.success('Retreived ' + resp.length + ' twitter names from meetup');
+    for (let i=0; i<resp.length; i++) {
+      // need to clean the names, some people write twitter.com/pezmc
+      let raw = resp[i];
+      if (raw.indexOf('\.com\/') !== -1) {
+        raw = raw.split('\.com\/')[1];
+      }
+      if (raw) cache.add('meetup', raw);
+    }
+    Log.info('Contacting twitter.com');
+    twit.getFollowedNames(function(err, resp) {
+      if (err) {
+        Log.error(err.message); 
+        return scheduleFollow(pollMinutes);
+      }
+        
+      Log.success('Retreived ' + resp.length + ' twitter names from twitter');
+      for (let i=0; i<resp.length; i++) {
+        cache.add('twitter', resp[i]);
+      }
+      let diff = cache.diff();
+      if (diff.length > 0) {
+        Log.info('Attempting to follow ' + diff.length + ' twitter names (' + diff.join() + ')');
+        twit.followNames(diff, function(err, resp) {
+          if (err) {
+            Log.error(err.message);
+            return scheduleFollow(pollMinutes);;
+          }
+
+          Log.success('All done!');
+          scheduleFollow(pollMinutes);
+        });
+      } else {
+        Log.success('Nobody to follow');
+        scheduleFollow(pollMinutes);
+      }
+    });
   });
 }
 
